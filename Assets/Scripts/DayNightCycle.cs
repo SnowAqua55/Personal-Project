8f366ad1a96a790171209b57e13624ba7b2d9163
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-19 11:35:11 +0900

    feat : Sun, Moon
    태양, 달 구현

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -10,8 +10,50 @@ public class DayNightCycle : MonoBehaviour
 	public Vector3 noon;
 
 	[Header("Sun")]
-
+	public Light sun;
+	public Gradient sunColor;
+	public AnimationCurve sunIntensity;
+	
 	[Header("Moon")]
-
+	public Light moon;
+	public Gradient moonColor;
+	public AnimationCurve moonIntensity;
+	
 	[Header("Other Lighting")]
+	public AnimationCurve lightingIntensityMultiplier;
+	public AnimationCurve reflectionIntensityMultiplier;
+
+	void Start()
+	{
+		timeRate = 1.0f / fullDayLength;
+		time = startTime;
+	}
+
+	void Update()
+	{
+		time = (time + timeRate * Time.deltaTime) % 1.0f;
+		
+		UpdateLighting(sun, sunColor, sunIntensity);
+		UpdateLighting(moon, moonColor, moonIntensity);
+		
+		// A
+		RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+		RenderSettings.ambientIntensity = reflectionIntensityMultiplier.Evaluate(time);
+	}
+	
+	void UpdateLighting(Light lightSource, Gradient colorGradient, AnimationCurve intensityCurve)
+	// Light : 광원, Gradient : 빛 색상 그라데이션, AnimationCurve : 그래프
+	{
+		float intensity = intensityCurve.Evaluate(time);
+		
+		lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4f;
+		lightSource.color = colorGradient.Evaluate(time);
+		lightSource.intensity = intensity;
+		
+		GameObject go = lightSource.gameObject;
+		if (lightSource.intensity == 0 && go.activeInHierarchy)
+			go.SetActive(false);
+		else if (lightSource.intensity > 0 && !go.activeInHierarchy)
+			go.SetActive(true);
+	}
 }
