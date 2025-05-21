a5e69abada2528b2ad1902b7fa834976083ed91c
SnowAqua55 <63825945+SnowAqua55@users.noreply.github.com>
2025-05-21 19:54:14 +0900

    feat : Food
    음식 구현 중
    UI에 함수 테스트용 버튼 하나 만들어 둠

---
+++ Assets/Scripts/Item/Food.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public interface IFoodEffectDuration
+{
+    void EatFood();  // 먹었을 때 효과 걸어주는 메서드
+}
+
+
+public class Food : MonoBehaviour
+{
+    // 0. 음식마다 달릴 스크립트
+    // 1. 음식에 스크립트 달고 인스펙터에서 효과 지정해 줘야 하니 그거 띄워줄 변수 선언
+    // 2. 
+
+    public void
+
+    public void OverTimeFood()
+    {
+        
+    }
+}
